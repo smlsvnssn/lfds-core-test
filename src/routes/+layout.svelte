21dc4f31cd741144371ed856243296ba74efa8d0
smlsvnssn <samuel@lhli.net>
2025-09-15 11:41:46 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<main>
+	{@render children?.()}
+</main>
+
+<style>
+	main {
+		display: grid;
+		gap: 1.5rem;
+		margin: auto;
+		max-width: 35rem;
+	}
+
+	:global(body) {
+		margin: 6rem 1rem;
+		margin-top: 6rem;
+	}
+</style>
